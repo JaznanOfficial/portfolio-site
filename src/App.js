bd95ade8536d4fa3691cec7bb671a0fe7880e4df
JaznanOfficial <jaznanofficial@gmail.com>
2021-11-30 16:38:30 +0600

    some changes

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Contact from "./Components/ContactMe/Contact";
 import Footer from "./Components/Footer/Footer";
 import Home from './Components/Home/Home';
 import Navigation from "./Components/Home/Navigation/Navigation";
+import AboutMe from "../src/Components/AboutMe/about";
 
 
 function App() {
@@ -21,6 +22,7 @@ function App() {
         <Routes>
           <Route exact path='/' element={<Home></Home>}></Route>
           <Route exact path='home' element={<Home></Home>}></Route>
+          <Route exact path='about' element={<AboutMe></AboutMe>}></Route>
           <Route exact path='contact' element={<Contact></Contact>}></Route>
           <Route exact path='blogs' element={<Blogs></Blogs>}></Route>
         </Routes>

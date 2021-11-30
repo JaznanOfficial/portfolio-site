7f93380c85ad1fd3b8a893225ffd15477483d2b7
JaznanOfficial <jaznanofficial@gmail.com>
2021-11-30 12:13:26 +0600

    responsive about

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,13 +1,16 @@
 import React from "react";
+import AboutMe from "../AboutMe/about";
 import Footer from "../Footer/Footer";
 import Navigation from "./Navigation/Navigation";
 import Profile from "./Profile/Profile";
+
 
 const Home = () => {
     return (
         <div>
             <Navigation></Navigation>
             <Profile></Profile>
+            <AboutMe></AboutMe>
             <Footer></Footer>
         </div>
     );

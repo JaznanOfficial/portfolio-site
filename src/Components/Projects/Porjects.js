de2bd86b6fb7b796d2012750deb070a44431be4a
JaznanOfficial <jaznanofficial@gmail.com>
2021-11-30 19:08:11 +0600

    alhamdulillah done

--- src/Components/Projects/Porjects.js
+++ src/Components/Projects/Porjects.js
@@ -22,9 +22,9 @@ const Porjects = () => {
                                     height='750px'
                                     title="please click to see details"
                                 />
-                            <Button className='m-1' variant="outline-dark"><strong>Live Site Link</strong></Button>
-                        <Button className='m-1' variant="outline-primary"> <strong>Client Side Code</strong> </Button>
-                        <Button className='m-1' variant="outline-success"> <strong>Server Side Code</strong> </Button>
+                            <Button href='https://car-deal-project.web.app/' target='_blank' className='m-1' variant="outline-dark"><strong>Live Site Link</strong></Button>
+                        <Button href='https://github.com/JaznanOfficial/car-deal-client-side' target='_blank' className='m-1' variant="outline-primary"> <strong>Client Side Code</strong> </Button>
+                        <Button href='https://github.com/JaznanOfficial/car-deal-server-side' target='_blank' className='m-1' variant="outline-success"> <strong>Server Side Code</strong> </Button>
                         </div>
                         <div className="card m-5 project-card" style={{ width: "18rem" }}>
                 
@@ -36,9 +36,9 @@ const Porjects = () => {
                                     title="please click to see details"
                                 />
                      
-                            <Button className='m-1' variant="outline-dark"><strong>Live Site Link</strong></Button>
-                        <Button className='m-1' variant="outline-primary"> <strong>Client Side Code</strong> </Button>
-                        <Button className='m-1' variant="outline-success"> <strong>Server Side Code</strong> </Button>
+                            <Button href='https://travel-and-tour-assignment.web.app/' target='_blank' className='m-1' variant="outline-dark"><strong>Live Site Link</strong></Button>
+                        <Button href='https://github.com/JaznanOfficial/tour-and-travel-client-side' target='_blank' className='m-1' variant="outline-primary"> <strong>Client Side Code</strong> </Button>
+                        <Button href='https://github.com/JaznanOfficial/tour-and-travel-server-side' target='_blank' className='m-1' variant="outline-success"> <strong>Server Side Code</strong> </Button>
                         </div>
                         <div className="card m-5 project-card" style={{ width: "18rem" }}>
                 
@@ -51,8 +51,8 @@ const Porjects = () => {
                                 />
                 
                             <Button href='https://first-eschool-project.netlify.app/' target="_blank" className='m-1' variant="outline-dark"><strong>Live Site Link</strong></Button>
-                        <Button className='m-1' variant="outline-primary"> <strong>Client Side Code</strong> </Button>
-                        <Button className='m-1' variant="outline-success"> <strong>Server Side Code</strong> </Button>
+                        <Button href='https://github.com/JaznanOfficial/e-learning' target='_blank' className='m-1' variant="outline-primary"> <strong>Client Side Code</strong> </Button>
+                        <Button className='m-1' variant="outline-success" disabled> <strong>Server Side Code</strong> </Button>
                         </div>
                     </Row>
                 </div>
